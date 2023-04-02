8aa548e5f39304383348cfc6508825613d2c421a
Gabriel Mattos Da Silva <gmtts123@gmail.com>
2023-04-02 01:24:34 -0300

    codigo fonte atualizado
    inserido documentação, tutorial, requirements

--- licensa.py
+++ licensa.py
@@ -4,10 +4,11 @@ from licensing.methods import Key, Helpers
 RSAPubKey = "xxx"## defina aqui sua chave rsa
 auth = "xxx" ## defina aqui sua authkey
 prod_id = 'xxx'#defina aqui seu id de produto da cryptolens
+
 def Authkey(key):
     result = Key.activate(token=auth,\
         rsa_pub_key=RSAPubKey,\
-        product_id=prod_id, \
+        product_id=prod_id,\
         key=key,\
         machine_code=Helpers.GetMachineCode())
 
@@ -17,7 +18,6 @@ def Authkey(key):
     else:
         print("Licenciamento validado atraves da api.")
         license_key = result[0]
-        print(result)
         expiration_date_formatted = license_key.expires.strftime('%d-%m-%Y %H:%M:%S')
         print("Licença expira: " + str(expiration_date_formatted))
         return True, license_key.expires
